e71c24a55b972c4c3624c9958df7ab5e012f09d9
antico94 <meiuandrei@gmail.com>
2023-03-22 15:07:44 +0200

    Added gitignore

---
+++ ProiectRelevance/Ghiozdan.cs
@@ -0,0 +1,8 @@
+namespace ProiectRelevance;
+
+public class Ghiozdan
+{
+    public int NumarArticoleMaxim { get; set; }
+    public int GreutateMaxima { get; set; }
+    public int VolumMaxim { get; set; }
+}
