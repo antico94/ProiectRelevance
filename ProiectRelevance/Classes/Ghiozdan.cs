5a2183511c409a3cd8ecb1635f1ea5fc5322f0a5
antico94 <meiuandrei@gmail.com>
2023-03-22 16:31:37 +0200

    Created Add method

--- ProiectRelevance/Classes/Ghiozdan.cs
+++ ProiectRelevance/Classes/Ghiozdan.cs
@@ -2,24 +2,36 @@ namespace ProiectRelevance.Classes;
 
 public class Ghiozdan
 {
-    public int NumarArticoleMaxim { get; set; }
-    public float GreutateMaxima { get; set; }
-    public float VolumMaxim { get; set; }
-    public List<ArticolInventar> Continut { get; set; }
-    public int NumarArticoleExistente { get; set; }
-    public float GreutateCurenta { get; set; }
-    public float VolumCurent { get; set; }
+    public int NumarArticoleMaxim { get; }
+    public float GreutateMaxima { get;}
+    public float VolumMaxim { get; }
+    private List<ArticolInventar> Continut { get; set; }
+    public int NumarArticoleExistente { get; private set; }
+    public float GreutateCurenta { get; private set; }
+    public float VolumCurent { get; private set; }
 
     public Ghiozdan(int numarArticoleMaxim, float greutateMaxima, float volumMaxim)
     {
         NumarArticoleMaxim = numarArticoleMaxim;
-        GreutateMaxima = GreutateMaxima;
-        VolumMaxim = VolumMaxim;
+        GreutateMaxima = greutateMaxima;
+        VolumMaxim = volumMaxim;
         NumarArticoleExistente = 0;
         GreutateCurenta = 0f;
         VolumCurent = 0f;
         Continut = new List<ArticolInventar>();
     }
-    
-    
+
+    public bool Adauga(ArticolInventar articol)
+    {
+        if (NumarArticoleExistente + 1 > NumarArticoleMaxim) return false;
+        if (GreutateCurenta + articol.Greutate > GreutateMaxima) return false;
+        if (VolumMaxim + articol.Volum > VolumMaxim) return false;
+
+        NumarArticoleExistente += 1;
+        GreutateCurenta += articol.Greutate;
+        VolumCurent += articol.Volum;
+        Continut.Add(articol);
+
+        return true;
+    }
 }
